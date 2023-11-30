99b313596ee882c83c1468d8c7e82ead21de7108
Praidis <110727718+Praidis@users.noreply.github.com>
2023-11-30 14:06:53 -0300

    BDatos

---
+++ src/main/java/BDLector/BD/Recursos/projectointegrador_ronda0.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: projectointegrador
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ronda`
+--
+
+DROP TABLE IF EXISTS `ronda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ronda` (
+  `idRonda` int NOT NULL,
+  `idPartido` varchar(45) NOT NULL,
+  `Equipo1` varchar(255) NOT NULL,
+  `Goles_Equipo1` int NOT NULL,
+  `Goles_Equipo2` int NOT NULL,
+  `Equipo2` varchar(255) NOT NULL,
+  PRIMARY KEY (`idRonda`,`idPartido`,`Equipo1`,`Goles_Equipo1`,`Goles_Equipo2`,`Equipo2`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ronda`
+--
+
+LOCK TABLES `ronda` WRITE;
+/*!40000 ALTER TABLE `ronda` DISABLE KEYS */;
+INSERT INTO `ronda` VALUES (1,'1','Argentina',3,2,'Brazil'),(1,'2','Chile',1,3,'Uruguay'),(1,'3','Peru',1,0,'Ecuador'),(1,'4','Venezuela',1,0,'Colombia'),(2,'1','Argentina',3,0,'Colombia'),(2,'2','Chile',1,2,'Ecuador'),(2,'3','Peru',0,3,'Uruguay'),(2,'4','Venezuela',0,0,'Brazil'),(2,'5','Croacia',1,1,'Alemania');
+/*!40000 ALTER TABLE `ronda` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-30 14:04:10
